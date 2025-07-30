4e762da2b7ac6201a756668eb1565544d1d37d7e
Svenja Nierwetberg <71074251+snierwetberg@users.noreply.github.com>
2025-07-30 12:00:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const switcher = document.querySelector('.btn');
 switcher.addEventListener('click', function(){
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('random-theme');
 
     const className = document.body.className;
     if(className == "light-theme"){
         this.textContent = "Dark";
+    } else if (className == "dark-theme") {
+        this.textContent = "Random";
     } else {
-        this.textContent = "Light";
-    }
+        this.textContent= "Light";
 
     console.log('current class name: ' + className);
 });
